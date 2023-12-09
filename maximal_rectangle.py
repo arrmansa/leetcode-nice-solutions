e3d9a587b79b147a3ff735431d9a368538de7b0e
arrmansa <41120982+arrmansa@users.noreply.github.com>
2023-12-10 04:39:09 +0530

    Create maximal_rectangle.py
    a solution for https://leetcode.com/problems/maximal-rectangle using np.all and a small truegroups function that works nicely

---
+++ maximal_rectangle.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        arr = np.array([[int(e) for e in l] for l in matrix], dtype = np.bool_)
+        val = 0
+        for c1 in range(arr.shape[0]):
+            not_small = lambda c2: (c2-c1)*arr.shape[1]>val
+            for c2 in filter(not_small, range(arr.shape[0], c1, -1)):
+                fullrows = np.all(arr[c1:c2], axis=0)
+                max_width = max(self.truegroups(fullrows))
+                val = max(val, max_width*(c2-c1))
+        return val
+    
+    @staticmethod
+    def truegroups(arr):
+        count = 0
+        for i in arr:
+            if i:
+                count += 1
+            elif count:
+                yield count
+                count = 0
+        yield count
